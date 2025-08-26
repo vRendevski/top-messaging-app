57d21a05dfa59dccf33c91c88081676f48d18414
vRendevski <73475170+vRendevski@users.noreply.github.com>
2025-08-26 21:52:51 +0200

    feat(frontend): Setup tanstack-router, tailwindcss, shadcn

---
+++ apps/frontend/src/routes/__root.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
+export const Route = createRootRoute({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <React.Fragment>
+      <div>Hello "__root"!</div>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </React.Fragment>
+  )
+}
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,22 @@
+import path from "path";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
