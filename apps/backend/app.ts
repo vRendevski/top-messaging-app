f01c6ab1f99d7003400e47f3c2914211c4a78166
vRendevski <73475170+vRendevski@users.noreply.github.com>
2025-08-29 19:05:08 +0200

    feat(backend): Use passport and error middleware in express server, add sessions to prisma schema

--- apps/backend/app.ts
+++ apps/backend/app.ts
@@ -1,15 +1,22 @@
 import "dotenv/config"
 import express from "express";
+import sessionMiddleware from "./src/config/session";
+import passport from "./src/config/passport";
 import apiRouter from "./src/router/apiRouter";
+import errorMiddleware from "./src/middleware/errorMiddleware";
 import wss from "./src/ws/gateway";
 
 const app = express();
 
 app.use(express.json());
+app.use(sessionMiddleware);
+app.use(passport.session());
 
 app.use("/api", apiRouter);
 
-const PORT = process.env.PORT;
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT ?? 8080;
 const listener = app.listen(PORT);
 
 listener.on("upgrade", () => wss.upgrade);
