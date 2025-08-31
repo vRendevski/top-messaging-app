fd21077df54f5b3e0f07093efd16f0f7c569b5c2
vRendevski <73475170+vRendevski@users.noreply.github.com>
2025-08-31 16:35:25 +0200

    feat(frontend): Remove landing page, redirect to signup instantly

--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -1,23 +1,12 @@
-import CenteredLayout from '@/components/layouts/CenteredLayout';
-import ButtonLink from '@/components/shared/ButtonLink';
 import { createFileRoute } from '@tanstack/react-router'
+import useRedirectIfAuthenticated from '@/hooks/useRedirectIfAuthenticated';
+import useRedirectIfUnauthenticated from '@/hooks/useRedirectIfUnauthenticated';
 
 export const Route = createFileRoute('/')({
   component: IndexComponent,
 })
 
 function IndexComponent() {
-  return (
-    <CenteredLayout>
-      <section className="p-4">
-        <h1 className="text-md md:text-2xl lg:text-3xl">
-          Messaging, Reimagined for Privacy
-        </h1>
-        <div className="mt-2 flex gap-2">
-          <ButtonLink to="/login">Login</ButtonLink>
-          <ButtonLink to="/signup">Signup</ButtonLink>
-        </div>
-      </section>
-    </CenteredLayout>
-  );
+  useRedirectIfAuthenticated("/chat");
+  useRedirectIfUnauthenticated("/login");
 }
